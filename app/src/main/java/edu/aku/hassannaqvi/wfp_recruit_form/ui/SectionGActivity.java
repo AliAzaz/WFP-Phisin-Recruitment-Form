78d1d6a71bd2c7c86e78300e08a9064091417f09
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-05-25 16:22:21 +0500

    Layouts

---
+++ app/src/main/java/edu/aku/hassannaqvi/wfp_recruit_form/ui/SectionGActivity.java
@@ -0,0 +1,13 @@
+package edu.aku.hassannaqvi.wfp_recruit_form.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SectionGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_g);
+    }
+}
